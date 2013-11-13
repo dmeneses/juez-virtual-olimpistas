a3fa11b90289f447bde889112bf33df4f4a1d8eb
Daniela Meneses <daniela11290@gmail.com>
2013-11-12 21:30:02 -0400

    Control training edition with authenticated users.

--- module/Training/src/Training/Controller/TrainingController.php
+++ module/Training/src/Training/Controller/TrainingController.php
@@ -8,16 +8,20 @@ use Training\Model\Training;
 use Training\Form\CreateTrainingForm;
 use Training\Form\AddProblemToTrainingForm;
 use Training\Form\AddGroupToTrainingForm;
-use Training\Model\DateValidator;
 
 class TrainingController extends AbstractActionController {
 
     protected $trainingTable;
     protected $problemTable;
     protected $groupTable;
+    protected $authService;
 
-    public function indexAction() {
-        return new ViewModel(array('trainings' => $this->getTrainingTable()->fetchAll()));
+    public function getAuthService() {
+        if (!$this->authService) {
+            $this->authService = $this->getServiceLocator()->get('AuthService');
+        }
+
+        return $this->authService;
     }
 
     public function getProblemTable() {
@@ -42,6 +46,10 @@ class TrainingController extends AbstractActionController {
             $this->groupTable = $sm->get('Group\Model\GroupTable');
         }
         return $this->groupTable;
+    }
+
+    public function indexAction() {
+        return new ViewModel(array('trainings' => $this->getTrainingTable()->fetchAll()));
     }
 
     public function createAction() {
@@ -79,33 +87,61 @@ class TrainingController extends AbstractActionController {
         if ($request->isPost()) {
             $postData = $request->getPost();
             if (isset($postData['addGroup'])) {
-                $groupForm->setData($postData);
-                $groupForm->setDbAdapter($this->getTrainingTable()->getDbAdapter());
-
-                if ($groupForm->isValid()) {
-                    $newGroup = $groupForm->get('group_id')->getValue();
-                    $this->getTrainingTable()->addGroup($training->training_id, $newGroup);
-                }
+                $this->addGroup($groupForm, $postData, $training);
             } else {
-                $problemForm->setData($postData);
-                $problemForm->setDbAdapter($this->getTrainingTable()->getDbAdapter());
-
-                if ($problemForm->isValid()) {
-                    $newProblem = $problemForm->get('problem_id')->getValue();
-                    $this->getTrainingTable()->addProblem($training->training_id, $newProblem);
-                }
+                $this->addProblem($problemForm, $postData, $training);
             }
         }
 
-        $groups = $this->getGroupTable()->getGroupsByTraining($id);
-        $data = array('problemForm' => $problemForm, 'groupForm' => $groupForm,
-            'trainingData' => $training, 'trainingGroups' => $groups);
-        if ($this->isEnabled($training->start_date, $training->start_time)) {
-            $problems = $this->getProblemTable()->getProblemsByTraining($id);
+        return $this->getDataToView($problemForm, $groupForm, $training);
+    }
+
+    private function getDataToView($problemForm, $groupForm, $training) {
+        $isOwner = $this->isTheOwner($training, $this->getAuthService()->getIdentity());
+        $groups = $this->getGroupTable()->getGroupsByTraining($training->training_id);
+        $data = array(
+            'problemForm' => $problemForm,
+            'groupForm' => $groupForm,
+            'trainingData' => $training,
+            'trainingGroups' => $groups,
+            'isOwner' => $isOwner,);
+
+        if ($this->isEnabled($training->start_date, $training->start_time) || $isOwner) {
+            $problems = $this->getProblemTable()->getProblemsByTraining($training->training_id);
             $data['trainingProblems'] = $problems;
         }
 
         return $data;
+    }
+
+    private function isTheOwner($training, $email) {
+        $userID = $this->trainingTable->getUserID($email);
+
+        if ($userID == $training->training_owner) {
+            return true;
+        }
+        
+        return false;
+    }
+
+    private function addProblem($problemForm, $postData, $training) {
+        $problemForm->setData($postData);
+        $problemForm->setDbAdapter($this->getTrainingTable()->getDbAdapter());
+
+        if ($problemForm->isValid()) {
+            $newProblem = $problemForm->get('problem_id')->getValue();
+            $this->getTrainingTable()->addProblem($training->training_id, $newProblem);
+        }
+    }
+
+    private function addGroup($groupForm, $postData, $training) {
+        $groupForm->setData($postData);
+        $groupForm->setDbAdapter($this->getTrainingTable()->getDbAdapter());
+
+        if ($groupForm->isValid()) {
+            $newGroup = $groupForm->get('group_id')->getValue();
+            $this->getTrainingTable()->addGroup($training->training_id, $newGroup);
+        }
     }
 
     public function isEnabled($startDate, $startTime) {
@@ -115,11 +151,11 @@ class TrainingController extends AbstractActionController {
         $date2 = strtotime(substr($startDate, 0, 10) . ' ' . $startTime);
         exec('echo ' . $date1 . ' > /tmp/hola');
         exec('echo ' . $date2 . ' >> /tmp/hola');
-        
-        if($date1 < $date2) {
+
+        if ($date1 < $date2) {
             return false;
         }
-        
+
         return true;
     }
 
