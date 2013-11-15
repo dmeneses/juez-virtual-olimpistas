242c5b47ee0f1d9685df5107024eb09c64e444b0
Daniela Meneses <daniela11290@gmail.com>
2013-11-15 10:45:59 -0400

    Initial group module test suite.

---
+++ module/Group/test/GroupTest/Controller/GroupControllerTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace GroupTest\Controller;
+
+use GroupTest\Bootstrap;
+use Group\Controller\GroupController;
+use Zend\Http\Request; 
+use Zend\Stdlib\Parameters;
+use Zend\Mvc\MvcEvent;
+use Zend\Mvc\Router\RouteMatch;
+use Zend\Mvc\Router\Http\TreeRouteStack as HttpRouter;
+use PHPUnit_Framework_TestCase;
+
+class GroupControllerTest extends PHPUnit_Framework_TestCase {
+
+    protected $controller;
+    protected $request;
+    protected $response;
+    protected $routeMatch;
+    protected $event;
+
+    protected function setUp() {
+        $serviceManager = Bootstrap::getServiceManager();
+        $this->controller = new GroupController();
+        $this->request = new Request();
+        $this->routeMatch = new RouteMatch(array('controller' => 'index'));
+        $this->event = new MvcEvent();
+        $config = $serviceManager->get('Config');
+        $routerConfig = isset($config['router']) ? $config['router'] : array();
+        $router = HttpRouter::factory($routerConfig);
+        $this->event->setRouter($router);
+        $this->event->setRouteMatch($this->routeMatch);
+        $this->controller->setEvent($this->event);
+        $this->controller->setServiceLocator($serviceManager);
+    }
+
+    public function testIndexActionCanBeAccessed() {
+        $this->routeMatch->setParam('action', 'index');
+
+        $result = $this->controller->dispatch($this->request);
+        $response = $this->controller->getResponse();
+
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testAddActionCanBeAccessed() {
+        $this->routeMatch->setParam('action', 'create');
+
+        $result = $this->controller->dispatch($this->request);
+        $response = $this->controller->getResponse();
+
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testAddActionWithEmptyParameters() {
+        $this->routeMatch->setParam('action', 'create');
+        $this->request
+                ->setMethod('POST')
+                ->setPost(new Parameters());
+        $this->controller->dispatch($this->request);
+        $response = $this->controller->getResponse();
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+       
+    public function testDisplayActionWithoutIdIsRedirected() {
+        $this->routeMatch->setParam('action', 'edit');
+
+        $result = $this->controller->dispatch($this->request);
+        $response = $this->controller->getResponse();
+
+        $this->assertEquals(302, $response->getStatusCode());
+    }
+
+    public function testDisplayActionWithWrongIdIsRedirected() {
+        $this->routeMatch->setParam('action', 'edit');
+        $this->routeMatch->setParam('id', 5000);
+
+        $result = $this->controller->dispatch($this->request);
+        $response = $this->controller->getResponse();
+
+        $this->assertEquals(302, $response->getStatusCode());
+    }
+
+}
