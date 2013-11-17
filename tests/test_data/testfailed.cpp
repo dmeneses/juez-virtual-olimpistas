4c9291b111d12201cbc3586d5197553a50594f2e
Daniela Meneses <daniela11290@gmail.com>
2013-11-17 16:49:42 -0400

    Adding tests and data to tests.

---
+++ tests/test_data/testfailed.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   test.cpp
+ * Author: Daniela Meneses
+ *
+ * Created on September 28, 2013, 2:57 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * Test file that will compile.
+ */
+int main(int argc, char** argv)
+{
+
+    return 0
+}
+
