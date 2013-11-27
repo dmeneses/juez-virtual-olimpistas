efc070f914d47f174f43afb429ea82fb3b4e7e7b
Daniela Meneses <daniela11290@gmail.com>
2013-11-27 09:56:28 -0400

    Adding test files for process monitor

---
+++ test_data/problem1/TimeLimitExceededSolution.cpp
@@ -0,0 +1,18 @@
+#include "iostream"
+
+using namespace std;
+
+int main() {
+	int n,a,b;
+	cin>> n;
+	
+	while(true){
+		cin >> a;
+		cin >> b;
+       		cout << (a+b) << '\n';
+		n--;
+	}
+
+	return 0;
+}
+
